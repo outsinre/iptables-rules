b89a15b234242eba21f0966d032df0bdec4f6f69
Outs Inre <outsinre@users.noreply.github.com>
2016-03-01 11:48:40 +0800

    check root account

--- iptables-general-rules.sh
+++ iptables-general-rules.sh
@@ -3,6 +3,15 @@
 # This is the script that will generate firewall rules for a client host.
 # Details refer to my blog.
 
+if [ `id -u` != "0" ]; then
+        echo "Error: you must be root to run this script!"
+        exit 1
+fi
+if [ $OSTYPE != "linux-gnu" ]; then
+        echo "Error: this script is only for Linux!"
+        exit 1
+fi
+
 iptables -F
 iptables -X
 iptables -Z
@@ -32,3 +41,6 @@ ip6tables -A INPUT -m conntrack --ctstate INVALID -j DROP
 ip6tables -A INPUT -s fe80::/10 -p ipv6-icmp -j ACCEPT
 ip6tables -A INPUT -p udp -m conntrack --ctstate NEW -j REJECT --reject-with icmp6-port-unreachable
 ip6tables -A INPUT -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -m conntrack --ctstate NEW -j REJECT --reject-with tcp-reset
+
+
+echo "Done!"
